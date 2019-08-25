b8eed4936bfdb3c99025cb0f3254d299fcdfa810
geodotka <dorota.kowalik@21s.pl>
2019-08-25 14:23:10 +0200

    Test functions from timer.js

--- test/specs/timer.spec.js
+++ test/specs/timer.spec.js
@@ -1,11 +1,58 @@
 const Timer = require('../../src/pomodoro/timer');
 
-console.log('Timer', Timer);
-
 describe('Timer', () => {
-    describe('minusFunct', () => {
-        it('......', () => {
-            console.log('---');
+    let $item = null;
+    beforeEach(() => {
+        $item = document.createElement('warsawjs-minus');
+    });
+    describe('decreaseElementNumber', () => {
+        it('should reduce value', () => {
+            // Arrange
+            $item.textContent = '14';
+            // Act
+            Timer.decreaseElementNumber($item);
+            // Assert
+            expect($item.textContent).toEqual('13');
+            // Teardown
+            // ...
+        });
+        it('should return MINIMUM_CLOCK_VALUE if put 0', () => {
+            // Arrange
+            $item.textContent = '0';
+            // Act
+            Timer.decreaseElementNumber($item);
+            // Assert
+            expect($item.textContent).toEqual('0');
+            // Teardown
+        });
+        it('should return MINIMUM_CLOCK_VALUE if put -1', () => {
+            // Arrange
+            $item.textContent = '-1';
+            // Act
+            Timer.decreaseElementNumber($item);
+            // Assert
+            expect($item.textContent).toEqual('0');
+            // Teardown
+        });
+    });
+    describe('increaseElementNumber', () => {
+        it('should increase value', () => {
+            // Arrange
+            $item.textContent = '14';
+            // Act
+            Timer.increaseElementNumber($item);
+            // Assert
+            expect($item.textContent).toEqual('15');
+            // Teardown
+        });
+        it('should return MAXIMUM_CLOCK_VALUE if put number too big', () => {
+            // Arrange
+            $item.textContent = '100';
+            // Act
+            Timer.increaseElementNumber($item);
+            // Assert
+            expect($item.textContent).toEqual('60');
+            // Teardown
         });
     });
 });
